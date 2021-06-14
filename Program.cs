63460ab29f5c480a5655c54d60a79c6b6b5da1e1
Jay <jay@jayonsoftware.com>
2021-06-14 14:53:08 -0400

    First Check In

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Markdig;
+using Markdig.Syntax;
+
+namespace Mdparser12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mdFile = File.ReadAllText("demo.md");
+            var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+            var astMarkdownDocument = Markdown.Parse(mdFile, pipeline);
+            foreach (var block in astMarkdownDocument)
+            {
+                if (block is Markdig.Syntax.ParagraphBlock)
+                {
+                    var paragrap = (ParagraphBlock) block;
+                    var lines = paragrap.Lines;
+                    Console.WriteLine(paragrap.Inline.FirstChild);
+                }
+            }
+
+        }
+    }
+}
