2ea02780cb1e97e07d4bd87f9184bf2f463c0b9d
Jay <jay@jayonsoftware.com>
2021-06-18 12:23:30 -0400

    Use an HTML Parser

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using AngleSharp;
+using AngleSharp.Html.Parser;
 using Markdig;
 using Markdig.Syntax;
 using Markdig.Syntax.Inlines;
+using Scriban;
 
 namespace Mdparser12
 {
@@ -11,17 +14,65 @@ namespace Mdparser12
     {
         static void Main(string[] _)
         {
-            var mdFile = File.ReadAllText("test.md");
-            var pipeline = new MarkdownPipelineBuilder()
-                .Use<YoutubeExtension>()
-                .UseAdvancedExtensions()
-                .Build();
+            //Use the default configuration for AngleSharp
+            var config = Configuration.Default;
 
-            var ast = Markdown.Parse(mdFile, pipeline);
-            var html = Markdown.ToHtml(ast, pipeline);
+            //Create a new context for evaluating webpages with the given config
+            var context = BrowsingContext.New(config);
 
-            Console.WriteLine(html);
+            var source = File.ReadAllText("demo.html");
+            var parser = context.GetService<IHtmlParser>();
+            var document = parser.ParseDocument(source);
+
+            var elements = document.QuerySelectorAll("figure");
+            foreach (var element in elements)
+            {
+                var html = element.InnerHtml;
+                if (html.Contains("class=\"source\""))
+                {
+                    //ToDo: call LinkRenderer, get the text and replace this element ?.
+                    //Console.WriteLine("Its You Tube");
+                }
+                else if (html.Contains("class=\"bookmark-info\"")) {
+
+                    foreach (var elementChildNode in element.ChildNodes)
+                    {
+                        Console.WriteLine(elementChildNode.NodeType);
+                        Console.WriteLine(elementChildNode.TextContent);
+                    }
+
+                    //Console.WriteLine("Its A book mark");
+                }
+            }
+         
+
+            //var mdFile = File.ReadAllText("test.md");
+            //var pipeline = new MarkdownPipelineBuilder()
+            //    .Use<YoutubeExtension>()
+            //    .UseAdvancedExtensions()
+            //    .Build();
+
+            //var ast = Markdown.Parse(mdFile, pipeline);
+            //var html = Markdown.ToHtml(ast, pipeline);
+
+            //Console.WriteLine(html);
             Console.ReadLine();
+        }
+
+        public static string LinkRenderer(String linkType, String title, String embedlink)
+        {
+            if (linkType == "YouTube")
+            {
+                var lmTemplate = File.ReadAllText("YouTube.sbnhtml");
+                var template = Template.Parse(lmTemplate);
+                var result = template.Render(new { embedlink, title });
+                return result;
+            }
+            else
+            {
+                return "<a href=\"" + embedlink + "\">" + title + "link text" + "</a>";
+
+            }
         }
     }
 }
