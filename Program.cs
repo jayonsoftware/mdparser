67467e75ee6378d0f092ea1f8517bc81e2b43ef9
yallie <yallie@yandex.ru>
2021-06-26 23:02:15 +0300

    Moved the youtube-related code to YoutubeLinkHelper, added template caching.

--- Program.cs
+++ Program.cs
@@ -14,6 +14,17 @@ namespace Mdparser12
 {
 	class Program
 	{
+		static void Main(string[] _)
+		{
+			// parse demo.html, save result.html
+			var source = File.ReadAllText("demo.html");
+			var result = YoutubeLinkHelper.ReplaceYoutubeLinks(source);
+			File.WriteAllText("result.html", result);
+
+			Console.WriteLine(result);
+			Console.ReadLine();
+		}
+
 		static void NodeParser(INode node)
 		{
 			Console.WriteLine(node.NodeType);
@@ -23,93 +34,6 @@ namespace Mdparser12
 				return;
 			}
 			NodeParser(nextNode);
-		}
-
-		static void Main(string[] _)
-		{
-			var source = File.ReadAllText("demo.html");
-			var result = ReplaceYoutubeLinks(source);
-			File.WriteAllText("result.html", result);
-			
-			Console.WriteLine(result);
-			Console.ReadLine();
-		}
-
-		private static string ReplaceYoutubeLinks(string html)
-		{
-			// parse the document
-			var config = Configuration.Default;
-			var context = BrowsingContext.New(config);
-			var parser = context.GetService<IHtmlParser>();
-			var document = parser.ParseDocument(html);
-
-			// find all youtube links
-			var links = document.All.Where(n => n.LocalName == "a").ToList();
-			foreach (var link in links)
-			{
-				// skip non-youtube links
-				var source = PrepareYoutubeVideo(link);
-				if (source == null)
-				{
-					// not a youtube link
-					continue;
-				}
-
-				// generate HTML fragment using the templating engine
-				var fragment = RenderYoutubeLink(source);
-				var nodes = parser.ParseFragment(fragment, link);
-
-				// replace the link with the rendered
-				link.ReplaceWith(nodes.ToArray());
-			}
-
-			// render the modified document
-			return document.DocumentElement.OuterHtml;
-		}
-
-		private static Source PrepareYoutubeVideo(IElement element)
-		{
-			var ignoreCase = StringComparison.OrdinalIgnoreCase;
-			if (element is IHtmlAnchorElement link)
-			{
-				if (link.Href.Contains("youtube.com", ignoreCase) ||
-					link.Href.Contains("youtu.be", ignoreCase))
-				{
-					// note: links don't have their own Ids
-					var src = new Source(string.Empty);
-					src.Description = link.Title ?? "Youtube video player";
-					src.URL = GetYoutubeEmbedLink(link.Href);
-					return src;
-				}
-			}
-
-			return null;
-		}
-
-		private static string GetYoutubeEmbedLink(string linkUrl)
-		{
-			// extract video identifier portion
-			var embedLink = linkUrl;
-			var url = new UriBuilder(linkUrl);
-
-			// assume something like https://youtu.be/HeQX2HjkcNo
-			if (url.Host.Contains("youtu.be", StringComparison.OrdinalIgnoreCase))
-			{
-				embedLink = $"https://www.youtube.com/embed{url.Path}";
-			}
-			else // assume something like https://www.youtube.com/watch?v=HeQX2HjkcNo
-			{
-				var queryParts = url.Query.Trim('?').Split('&');
-				foreach (var part in queryParts)
-				{
-					if (part.StartsWith("v=", StringComparison.OrdinalIgnoreCase))
-					{
-						embedLink = $"https://www.youtube.com/embed/{part[2..].Trim()}";
-					}
-				}
-			}
-
-			return embedLink;
 		}
 
 		// Given an html, parse it and return a List<Node> 
@@ -155,22 +79,6 @@ namespace Mdparser12
 		{
 			//Todo : Append the given html to the correct postion. 
 			return html;
-		}
-
-
-		// All Link is managed here, returns an html back
-		public static string RenderYoutubeLink(Source link)
-		{
-			var lmTemplate = File.ReadAllText("YouTube.sbnhtml");
-			var template = Template.ParseLiquid(lmTemplate);
-			var result = template.Render(new
-			{
-				test = "Cool",
-				embedlink = link.URL,
-				title = link.Description
-			});
-
-			return result;
 		}
 	}
 }
---
+++ YoutubeLinkHelper.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using Markdig;
+using Markdig.Renderers;
+using Markdig.Renderers.Html.Inlines;
+using Markdig.Syntax.Inlines;
+using Scriban;
+
+namespace Mdparser12
+{
+	/// <summary>
+	/// Youtube link helper.
+	/// </summary>
+	public class YoutubeLinkHelper
+	{
+		/// <summary>
+		/// Replaces links to Youtube videos with iframe code.
+		/// </summary>
+		public static string ReplaceYoutubeLinks(string html)
+		{
+			// parse the document
+			var config = Configuration.Default;
+			var context = BrowsingContext.New(config);
+			var parser = context.GetService<IHtmlParser>();
+			var document = parser.ParseDocument(html);
+
+			// find all youtube links
+			var links = document.All.Where(n => n.LocalName == "a").ToList();
+			foreach (var link in links)
+			{
+				// skip non-youtube links
+				var source = PrepareYoutubeVideo(link);
+				if (source == null)
+				{
+					// not a youtube link
+					continue;
+				}
+
+				// generate HTML fragment using the templating engine
+				var fragment = RenderYoutubeLink(source);
+				var nodes = parser.ParseFragment(fragment, link);
+
+				// replace the link with the rendered
+				link.ReplaceWith(nodes.ToArray());
+			}
+
+			// render the modified document
+			return document.DocumentElement.OuterHtml;
+		}
+
+		/// <summary>
+		/// Detects if the given HTML element is a youtube link and if it's so, produces a model class out of it.
+		/// </summary>
+		private static Source PrepareYoutubeVideo(IElement element)
+		{
+			var ignoreCase = StringComparison.OrdinalIgnoreCase;
+			if (element is IHtmlAnchorElement link)
+			{
+				if (link.Href.Contains("youtube.com", ignoreCase) ||
+					link.Href.Contains("youtu.be", ignoreCase))
+				{
+					// note: links don't have their own Ids
+					var src = new Source(string.Empty);
+					src.Description = link.Title ?? "Youtube video player";
+					src.URL = GetYoutubeEmbedLink(link.Href);
+					return src;
+				}
+			}
+
+			return null;
+		}
+
+		/// <summary>
+		/// Transforms youtube shareable video link into an embeddable link.
+		/// </summary>
+		private static string GetYoutubeEmbedLink(string linkUrl)
+		{
+			// extract video identifier portion
+			var embedLink = linkUrl;
+			var url = new UriBuilder(linkUrl);
+
+			// assume something like https://youtu.be/HeQX2HjkcNo
+			if (url.Host.Contains("youtu.be", StringComparison.OrdinalIgnoreCase))
+			{
+				embedLink = $"https://www.youtube.com/embed{url.Path}";
+			}
+			else // assume something like https://www.youtube.com/watch?v=HeQX2HjkcNo
+			{
+				var queryParts = url.Query.Trim('?').Split('&');
+				foreach (var part in queryParts)
+				{
+					if (part.StartsWith("v=", StringComparison.OrdinalIgnoreCase))
+					{
+						embedLink = $"https://www.youtube.com/embed/{part[2..].Trim()}";
+					}
+				}
+			}
+
+			return embedLink;
+		}
+
+		/// <summary>
+		/// Lazy-evaluated Youtube iframe template.
+		/// </summary>
+		private static Lazy<Template> YoutubeTemplate { get; } = new Lazy<Template>(() =>
+		{
+			var lmTemplate = File.ReadAllText("YouTube.sbnhtml");
+			return Template.Parse(lmTemplate);
+		});
+
+		/// <summary>
+		/// Renders a Youtube iframe fragment.
+		/// </summary>
+		private static string RenderYoutubeLink(Source link)
+		{
+			return YoutubeTemplate.Value.Render(new
+			{
+				embedlink = link.URL,
+				title = link.Description
+			});
+		}
+	}
+}
